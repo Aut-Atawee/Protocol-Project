7518dbee5afc3b01aa04b53d70a6bcf3da0390d2
Aut_Atawee <autist_1210@hotmail.com>
2021-02-11 22:18:36 +0700

    Add KFN Protocol

--- client.py
+++ client.py
@@ -1,17 +1,50 @@
 import socket
+import json
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 1234))
 
-name = input("Name : ")
-s.send(bytes(name,"utf-8"))
-data = s.recv(1024)
-print(repr(str(data,"utf-8")))
+try :
+    s.connect(("localhost", 8009))
+except socket.error :
+    print("Fail Connection")
+    sys.exit()
+
+name = input("Enter your name : ")
+data = {
+    "protocol":"KFN",
+    "status":"747 Hello",
+    "name":name
+}
+data = bytes(json.dumps(data),encoding="utf-8")
+s.send(data)
+
+
 while True :
-    s.send(int(input()).to_bytes(2,byteorder="big"))
-    data = s.recv(1024)
-    print(repr(str(data,"utf-8")))
-    if ((str(data,"utf-8")) == "Congratulations! Your guess is correct!") :
+
+    receive = s.recv(1024)
+    receive = json.loads(receive.decode("utf-8"))
+    print(receive["message"])
+
+    if receive["status"] == "350 Again" :
+        guess = int(input("Enter your guess (0-100) : "))
+        sendto_server = {
+            "protocol":"KFN",
+            "status":"747 Guess",
+            "guess":guess
+        }
+        sendto_server = bytes(json.dumps(sendto_server),encoding="utf-8")
+        s.send(sendto_server)
+    elif receive["status"] == "340 Close" :
+        print("End connection")
+        s.close()
         break
-s.close()
-print('End')
+
+    # guess = int(input("Enter your guess (0-100) : "))
+    
+    # s.send(guess.to_bytes(2, byteorder="big"))
+    
+    # data = s.recv(1024)
+    # print(repr(str(data,"utf-8")))
+    # if ((str(data,"utf-8")) == "Congratulations! Your guess is correct!") :
+    #     break
--- server.py
+++ server.py
@@ -1,35 +1,88 @@
 import socket
 import random
+import json
 import math
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("localhost", 1234))
+s.bind(("localhost", 8009))
 s.listen()
 
-target_number = math.floor(random.random() * 100)
 while True :
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
 
-    data = clientsocket.recv(1024)    
-    clientsocket.sendall(bytes("Welcome " + str(data,"utf-8") +" to Koisuru Fortune Number!" ,"utf-8"))
+    try :
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established!")
+    except socket.error :
+        print("Fail Connection")
+        continue
+
+    target_number = math.floor(random.random() * 100)
+    # print(target_number)
+
     
+    receive = clientsocket.recv(1024)
+    receive = json.loads(receive.decode("utf-8"))
+    
+    sendto_client = {
+        "protocol" : "KFN" ,
+        "status" : "350 Again" ,
+        "message" : "Welcome " + receive["name"] + " to Koisuru Fortune Number!"
+    }
+
+    sendto_client = bytes(json.dumps(sendto_client),encoding="utf-8")
+    clientsocket.send(sendto_client)
+
+
     while True :
 
-        guess_number = clientsocket.recv(1024)
-        # clientsocket.send(bytes("Enter your guess (0-100) : " + clientsocket.recv(1024)), "utf-8")
-        
+        receive_from_client = clientsocket.recv(1024)
+        data = json.loads(receive_from_client.decode("utf-8"))
 
-        if (int.from_bytes(guess_number,byteorder="big") == target_number) :
-            clientsocket.send(bytes("Congratulations!"+" Your guess is correct!","utf-8"))
-            break
-        elif int.from_bytes(guess_number,byteorder="big") > 100 or int.from_bytes(guess_number,byteorder="big") < 0 :
-            clientsocket.send(bytes("Your guess is out of range","utf-8"))
-        elif int.from_bytes(guess_number,byteorder="big") < target_number :
-            clientsocket.send(bytes("Your guess is too low, Please try again!","utf-8"))
-        elif int.from_bytes(guess_number,byteorder="big") > target_number :
-            clientsocket.send(bytes("Your guess is too high, Please try again!","utf-8"))
     
-    print("End")
+        if (0 >= data["guess"] >= 100) :
+            sendto_client = {
+                "protocol":"KFN",
+                "status":"350 Again",
+                "message":"Your guess is out of range, Please try again!"
+            }
+            sendto_client = bytes(json.dumps(sendto_client),encoding="utf-8")
+            clientsocket.send(sendto_client)
+        elif data["guess"] == target_number :
+            sendto_client = {
+                "protocol":"KFN",
+                "status":"340 Close",
+                "message":"Congratulations! Your guess is correct!"
+            }
+            sendto_client = bytes(json.dumps(sendto_client),encoding="utf-8")
+            clientsocket.send(sendto_client) 
+            break  
+        elif data["guess"] > target_number :
+            sendto_client = {
+                "protocol":"KFN",
+                "status":"350 Again",
+                "message":"Your guess is too high, Please try again!"
+            }
+            sendto_client = bytes(json.dumps(sendto_client),encoding="utf-8")
+            clientsocket.send(sendto_client)
+        elif data["guess"] < target_number :
+            sendto_client = {
+                "protocol":"KFN",
+                "status":"350 Again",
+                "message":"Your guess is too low, Please try again!"
+            }
+            sendto_client = bytes(json.dumps(sendto_client),encoding="utf-8")
+            clientsocket.send(sendto_client)
+        else :
+            sendto_client = {
+                "protocol":"KFN",
+                "status":"350 Again",
+                "message":"Invalid input! Please input numeric 0-100"
+            }
+            sendto_client = bytes(json.dumps(sendto_client),encoding="utf-8")
+            clientsocket.send(sendto_client)
+
+    
+    print("End connection with " + str(data))
+    # print()
     clientsocket.close()
 clientsocket.close()
